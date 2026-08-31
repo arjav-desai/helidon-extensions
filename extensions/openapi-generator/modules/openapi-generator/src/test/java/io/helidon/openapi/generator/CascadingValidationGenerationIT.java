0671ac73360a2850781a1ca1375dc904e68fc41b
Arjav Desai <arjav.desai@oracle.com>
2026-08-31 10:26:29 -0500

    Upgrade OpenAPI generator to Helidon 4.5.4 (#128)

--- extensions/openapi-generator/modules/openapi-generator/src/test/java/io/helidon/openapi/generator/CascadingValidationGenerationIT.java
+++ extensions/openapi-generator/modules/openapi-generator/src/test/java/io/helidon/openapi/generator/CascadingValidationGenerationIT.java
@@ -515,7 +515,7 @@ class CascadingValidationGenerationIT {
                 .setGeneratorName("helidon-declarative")
                 .setInputSpec(inputSpec)
                 .setOutputDir(target.toString())
-                .addAdditionalProperty("helidonVersion", "4.5.2")
+                .addAdditionalProperty("helidonVersion", "4.5.4")
                 .addAdditionalProperty("apiPackage", "io.helidon.example.api")
                 .addAdditionalProperty("modelPackage", "io.helidon.example.model")
                 .addAdditionalProperty("invokerPackage", "io.helidon.example");
